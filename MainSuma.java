4599d8281687687576df525031c1f6fd6a0f4456
Enrique Juarez <enriquejuarez.ej7@gmail.com>
2024-04-22 22:54:28 -0600

    suma statica

---
+++ MainSuma.java
@@ -0,0 +1,8 @@
+public class MainSuma {
+    public static void main(String[]args){
+        Suma opc = new Suma ();
+        opc.a=3;
+        opc.b=4;
+        System.out.println(opc.sumar());
+    }
+}
