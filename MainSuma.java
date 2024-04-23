f2df4828bce32815e4c9ed27ea3bb496fbc8f6ed
Enrique Juarez <enriquejuarez.ej7@gmail.com>
2024-04-22 23:07:53 -0600

    suma interactiva

--- MainSuma.java
+++ MainSuma.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
 public class MainSuma {
     public static void main(String[]args){
-        Suma opc = new Suma ();
-        opc.a=3;
-        opc.b=4;
-        System.out.println(opc.sumar());
+        Scanner opc = new Scanner(System.in);
+        System.out.println("Ingresa un numero");
+        int a= opc.nextInt();
+        System.out.println("Ingresa otro numero");
+        int b= opc.nextInt();
+        int r= a+b;
+        System.out.println("El resultado de la suma es:"+r);
     }
 }
--- Suma.java
+++ Suma.java
@@ -1,6 +1,6 @@
 public class Suma {
     int a,b,r;
-    public int sumar(){
+    public int sumar(int a, int b){
     r= this.a + this.b;
     return r;
     }
